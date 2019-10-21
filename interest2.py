ad249f574f5e1e50ee6d05f93bbd9e978d3c62db
ctrlaltflex <35972776+ctrlaltflex@users.noreply.github.com>
2019-10-21 18:46:55 -0400

    Add files via upload

---
+++ interest2.py
@@ -0,0 +1,47 @@
+
+import math
+
+def simple_inputs():
+    print('How many years will you be investing?')
+    years = int(input('Enter years: '))
+    print('What is the principle amount?')
+    principle = float(input('Enter amount: '))
+    print('What will your yearly interest be on your investment?')
+    interest = float(input('Enter your yearly interest: '))
+
+    total_interest = (principle*years*interest)/100
+    total = total_interest+principle
+
+    print('Your total after {} years will be $'.format(years) + str(total))
+
+
+
+def compound_interest(principle, rate, time):
+    result = principle * (pow((1 + rate / 100), time))
+    return result
+ 
+def result1():
+    p = float(input("Enter the principal amount: "))
+    r = float(input("Enter the interest rate: "))
+    t = float(input("Enter the time in years: "))
+    
+    amount = compound_interest(p, r, t)
+    interest = amount - p
+    print("Compound amount is %.2f" % amount)
+    print("Compound interest is %.2f" % interest)
+
+print('Would you like to calculate the simple interest, or compounding interest on the loan?')
+type_int = input(print('Enter "simple" or "compounding":'))
+
+
+if type_int.lower() == "simple":
+    simple_inputs()
+elif type_int.lower() == "compounding":
+    compound_interest(0,0,0),
+    result1()
+else:
+    print('That is not a valid entry.')
+
+if __name__ == "__main__":
+    pass
+
